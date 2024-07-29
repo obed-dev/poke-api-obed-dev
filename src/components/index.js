559904a8f25b51cc516239c8f9d1b3e513e2a3d8
[Obed Baltodano] <obedbaltodano23@homtail.com>
2024-07-28 20:02:00 -0600

    new changes scroll bar color

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+ 
+
+
+ export * from '../components/LoadMorePokemons';
+ export * from '../components/ScrollUp';
+ export * from '../components/SearchingPokemon';
+ export * from '../components/DataPokemon';
+ export * from '../components/AppPokemon';
